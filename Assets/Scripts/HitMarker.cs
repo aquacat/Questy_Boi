16ff396ed9ae57c6faedf1a52a03a1cdcef7cfa9
Noah Anderson <andernoha@gmail.com>
2018-11-10 21:45:39 -0600

    Initial commit

---
+++ Assets/Scripts/HitMarker.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+
+
+
+
+public class HitMarker : MonoBehaviour
+
+{
+
+
+
+    private Rigidbody rb;
+
+    private object other;
+
+
+
+    void Start()
+
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+
+    public void OnCollisionEnter(Collision other)
+
+    {
+
+        if (other.gameObject.CompareTag("Dummie 1"))
+
+        {
+
+            other.gameObject.SetActive(false);
+
+        }
+
+        if (other.gameObject.CompareTag("Dummie 2"))
+
+        {
+
+            other.gameObject.SetActive(false);
+
+        }
+
+        if (other.gameObject.CompareTag("Dummie 3"))
+
+        {
+
+            other.gameObject.SetActive(false);
+
+        }
+
+        if (other.gameObject.CompareTag("Dummie 4"))
+
+        {
+
+            other.gameObject.SetActive(false);
+
+        }
+
+        if (other.gameObject.CompareTag("Dummie 5"))
+
+        {
+
+            other.gameObject.SetActive(false);
+
+        }
+
+        if (other.gameObject.CompareTag("Dummie 6"))
+
+        {
+
+            other.gameObject.SetActive(false);
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/SimpleGameManager.cs
@@ -0,0 +1,213 @@
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public enum GameState { MAIN_MENU, GAME, EXIT }
+
+public delegate void OnStateChangeHandler();
+
+public class SimpleGameManager : MonoBehaviour
+
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public bool Quest1 = false;
+
+    public bool Quest2 = false;
+
+    public bool Quest3 = false;
+
+    public bool Quest4 = false;
+
+    public bool Quest5 = false;
+
+    public bool Quest6 = false;
+    private Dictionary<string, bool> questStates = new Dictionary<string, bool>();
+
+    private Dictionary<string, string> questDescriptions = new Dictionary<string, string>();
+    protected SimpleGameManager()
+    {
+
+        questStates.Add("Dummie 1", false);
+
+        questDescriptions.Add("Dummie 1", "Killed the red-haired brute holding an axe!");
+
+        questStates.Add("Dummie 2", false);
+
+        questDescriptions.Add("Dummie 2", "Smoked the little imp!");
+
+        questStates.Add("Dummie 3", false);
+
+        questDescriptions.Add("Dummie 3", "Annihilated the alien trooper!");
+
+        questStates.Add("Dummie 4", false);
+
+        questDescriptions.Add("Dummie 4", "BOI DOWN!");
+
+        questStates.Add("Dummie 5", false);
+
+        questDescriptions.Add("Dummie 5", "Good Shot!");
+        
+        questStates.Add("Dummie 6", false);
+
+        questDescriptions.Add("Dummie 6", "Dummie Downed!");
+
+        Debug.Log("Added Quests for Dummie 1, Dummie 2, Dummie 3, and the rest.");
+
+    }
+    void SetCountText ()
+    {
+        int count = 0;
+        foreach(bool quest in questStates.Values){
+            if (quest){
+                count ++;
+            }
+        }
+        countText.text = "Kills: " + count.ToString();
+    }
+
+    private static SimpleGameManager instance = null;
+
+    public event OnStateChangeHandler OnStateChange;
+
+    public GameState gameState { get; private set; }
+
+    public static SimpleGameManager Instance
+
+    {
+
+        get
+
+        {
+
+            if (SimpleGameManager.instance == null)
+
+            {
+
+                SimpleGameManager.instance = FindObjectOfType<SimpleGameManager>();
+
+                if (SimpleGameManager.instance == null)
+                {
+
+                    GameObject go = new GameObject();
+
+                    go.name = "SimpleGameManager";
+
+                    SimpleGameManager.instance = go.AddComponent<SimpleGameManager>();
+
+                    DontDestroyOnLoad(go);
+
+                }
+
+            }
+
+            return SimpleGameManager.instance;
+
+        }
+
+
+
+    }
+
+    void Awake()
+
+    {
+
+        if (instance == null)
+        {
+
+            instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void SetGameState(GameState state)
+
+    {
+
+        this.gameState = state;
+
+        OnStateChange();
+
+    }
+
+    public void OnApplicationQuit()
+
+    {
+
+        SimpleGameManager.instance = null;
+
+    }
+
+    public void FinishQuest(string questName)
+    {
+
+        Debug.Log("Finishing Quest for " + questName);
+
+        if (questStates.ContainsKey(questName))
+        {
+
+            questStates[questName] = true;
+            SetCountText();
+
+        }
+        if (AreQuestsFinished ()){
+            OpenTheGate();
+            SetCountText();
+        }
+
+    }
+
+
+
+    public string GetQuestDescription(string questName)
+    {
+
+        string desc = "";
+
+        questDescriptions.TryGetValue(questName, out desc);
+
+        return desc;
+
+    }
+
+    public bool AreQuestsFinished()
+    {
+
+
+        return !questStates.ContainsValue(false);
+    }
+
+    void OpenTheGate()
+    {
+
+        GameObject[] gates;
+
+        gates = GameObject.FindGameObjectsWithTag("EndWall");
+
+        Debug.Log("Got " + gates.Length + " gates");
+
+        foreach (GameObject gate in gates)
+
+        {
+
+            Destroy(gate);
+
+        }
+
+    }
+
+}
